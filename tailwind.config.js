d072ba713a4669490edbf250f72cae5c05d1751f
Silas Joisten <silasjoisten@proton.me>
2024-01-04 14:05:30 +0200

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,15 @@ module.exports = {
         'poppins': ['Poppins', 'sans-serif'],
         'montserrat': ['Montserrat', 'sans-serif'],
       },
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: {transform: 'translateX(0)'},
+          to: {transform: 'translateX(-100%)'},
+        }
+      },
     },
   },
   plugins: [],
